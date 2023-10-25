87ec0d0f5af72ae75855f508321cdbcbc4a3cd05
mpluiz <marcospaulo.luiz@gmail.com>
2023-10-25 14:40:46 -0300

    feat(presentation/component): add Loader

---
+++ src/presentation/components/Loader/Loader.tsx
@@ -0,0 +1,13 @@
+import '@/presentation/components/Loader/Loader.scss'
+
+type LoaderProps = { size?: 'sm' | 'md' | 'lg' | 'display' }
+
+export function Loader({ size = 'md' }: LoaderProps) {
+  return (
+    <span className={`loader loader--size-${size}`} role="status">
+      <svg viewBox="0 0 80 80">
+        <circle cx="40" cy="40" r="32"></circle>
+      </svg>
+    </span>
+  )
+}
