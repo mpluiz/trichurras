116a438d02becd8a130fdc1c54882113a53eb8ca
mpluiz <marcospaulo.luiz@gmail.com>
2023-10-25 13:53:13 -0300

    test(factories): update attributes of barbecue and participant

--- tests/factories/make-barbecue.ts
+++ tests/factories/make-barbecue.ts
@@ -1,15 +1,10 @@
 import { Barbecue, BarbecueProps } from '@/domain/entities'
-import { makeParticipant } from '@tests/factories/make-participant.ts'
 import { Money } from '@/domain/entities/value-objects'
 
 export function makeBarbecue(override: Partial<BarbecueProps> = {}): Barbecue {
-  const participants = [makeParticipant()]
   const barbecue = Barbecue.create({
-    id: '1',
     title: 'valid-title',
-    description: 'valid-description',
     notes: 'valid-notes',
-    participants,
     suggestedValue: { withDrinks: new Money(50), withoutDrinks: new Money(30) },
     date: new Date(),
     createdAt: new Date(),
