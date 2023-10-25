ec9734883bcac0fe7f6a2f3ed4c9cafa964b104d
mpluiz <marcospaulo.luiz@gmail.com>
2023-10-25 14:54:06 -0300

    feat(main): implements MainRouter

--- src/main/index.tsx
+++ src/main/index.tsx
@@ -1,8 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
+import '@/presentation/assets/styles/normalize.css'
+import '@/presentation/assets/styles/global.css'
+import '@/presentation/assets/styles/variables.css'
+
+import { MainRouter } from '@/main/routes/router.tsx'
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <div>TRICHURRAS</div>
+    <MainRouter />
   </React.StrictMode>
 )
