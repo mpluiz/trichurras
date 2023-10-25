69cce698c97bc5874cc82ae283bf4d80981e4276
mpluiz <marcospaulo.luiz@gmail.com>
2023-10-25 14:29:41 -0300

    chore(storybook): add global styles in storybook config

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,5 +1,9 @@
 import type { Preview } from "@storybook/react";
 
+import '@/presentation/assets/styles/normalize.css'
+import '@/presentation/assets/styles/global.css'
+import '@/presentation/assets/styles/variables.css'
+
 const preview: Preview = {
   parameters: {
     actions: { argTypesRegex: "^on[A-Z].*" },
@@ -8,6 +12,14 @@ const preview: Preview = {
         color: /(background|color)$/i,
         date: /Date$/,
       },
+    },
+    backgrounds: {
+      default: 'yellow',
+      values: [
+        { name: 'yellow', value: '#FFD836' },
+        { name: 'white', value: '#FFFFFF' },
+        { name: 'black', value: '#000000' },
+      ],
     },
   },
 };
